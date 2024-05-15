a69d1474737adf04633a647f07f345ff723f388e
Samara Caldas <samaracaldass@hotmail.com>
2024-05-15 11:57:29 -0300

    creating navbar and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        <h3>Escreva sobre o livro que você está lendo!</h3>
+        <p>Shelf Blog &copy; 2024</p>
+    </footer>
+  )
+}
+
+export default Footer
