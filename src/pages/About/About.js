38232c21d6293cac39f5520833bd9efe82a47354
Samara Caldas <samaracaldass@hotmail.com>
2024-05-16 11:24:06 -0300

    auth and about page
    blocking pages that require a login to be accessed and adding content from the about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,20 @@
 import styles from "./About.module.css";
 
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
-    <div>
-        <h1>About</h1>
+    <div className={styles.about}>
+        <h2>
+          Sobre o Shelf <span>Blog</span>
+        </h2>
+        <p>
+          Este projeto consiste em um blog feito com React no front-end e 
+          Firebase no back-end.
+        </p>
+        <Link to="/posts/create" className="btn">
+          Criar Post
+        </Link>
     </div>
   )
 }
