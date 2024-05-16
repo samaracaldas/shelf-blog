6064a8522b72bcdffb6d0e5b3db9b5929158de0a
Samara Caldas <samaracaldass@hotmail.com>
2024-05-16 10:41:22 -0300

    register and auth settings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+
+// hooks
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
+
+// context
+import { AuthProvider } from './context/AuthContext';
 
 // pages
 import Home from './pages/Home/Home';
@@ -8,23 +16,44 @@ import About from './pages/About/About';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Login from './pages/Login/Login';
-import Register from './pages/Login/Register/Register';
+import Register from './pages/Register/Register';
 
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const { auth } = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+
+  }, [auth])
+
+  if (loadingUser) {
+    return <p>Carregando...</p>
+
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/about' element={<About />} />
-                <Route path='/' element={<Login />} />
-                <Route path='/register' element={<Register />} />
-            </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider value={{user}}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+              <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/about' element={<About />} />
+                  <Route path='/' element={<Login />} />
+                  <Route path='/register' element={<Register />} />
+              </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
