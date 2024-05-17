c466b5d9263f128eb615f262514e96e2eb62c76d
Samara Caldas <samaracaldass@hotmail.com>
2024-05-17 10:55:09 -0300

    fetch documents and post
    searching for documents and displaying posts on the home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,13 +3,15 @@ import styles from "./Home.module.css";
 // hooks
 import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 // components
+import PostDetail from "../../components/PostDetail";
 
 
 const Home = () => {
   const [ query, setQuery ] = useState("");
-  const [posts] = useState([])
+  const { documents: posts, loading } = useFetchDocuments("posts");
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -28,7 +30,11 @@ const Home = () => {
       </form>
 
       <div>
-        <h1>Posts...</h1>
+        {loading && <p>Carregando...</p>}
+
+        {posts && posts.map((post) => (
+          <PostDetail post={post} />
+        ))}
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
