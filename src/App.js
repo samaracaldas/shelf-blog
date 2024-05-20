431701e1ca11243111c4353a653162028e3361fd
Samara Caldas <samaracaldass@hotmail.com>
2024-05-20 15:39:58 -0300

    search functionality
    creation of search functionality and display of results

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Search from './pages/Search/Search';
 
 function App() {
 
@@ -49,10 +50,23 @@ function App() {
               <Routes>
                   <Route path='/' element={<Home />} />
                   <Route path='/about' element={<About />} />
-                  <Route path='/login' element={!user ? <Login /> : <Navigate to="/" />} />
-                  <Route path='/register' element={!user ?<Register /> : <Navigate to="/" />} />
-                  <Route path='/posts/create' element={user ? <CreatePost/> : <Navigate to="/login" />} />
-                  <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate to="/login" />} />
+                  <Route path='/search' element={<Search />} />
+                  <Route
+                      path='/login' 
+                      element={!user ? <Login /> : <Navigate to="/" />} 
+                   />
+                  <Route 
+                    path='/register' 
+                    element={!user ?<Register /> : <Navigate to="/" />}
+                  />
+                  <Route
+                    path='/posts/create'
+                    element={user ? <CreatePost/> : <Navigate to="/login" />}
+                  />
+                  <Route 
+                    path='/dashboard'
+                    element={user ? <Dashboard /> : <Navigate to="/login" />} 
+                  />
               </Routes>
           </div>
           <Footer />
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,18 +13,24 @@ const Home = () => {
   const [ query, setQuery ] = useState("");
   const { documents: posts, loading } = useFetchDocuments("posts");
 
+  const navigate =useNavigate()
+
   const handleSubmit = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+
+    if (query) {
+      return navigate(`/search?q=${query}`);
+    }
+  };
 
   return (
     <div className={styles.home}>
       <h1>Posts mais recentes</h1>
-      <form onSubmit={handleSubmit} className={styles.search_form}>
-        <input 
-          type="text" 
-          placeholder="Ou busque por tags..." 
-          onChange={(e) => setQuery(e.target.value)} 
+      <form className={styles.search_form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button className="btn btn-dark">Pesquisar</button>
       </form>
